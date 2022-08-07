ce835a563312e1a2cefb33ba98089e894eef0ecb
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-08-07 13:30:09 +0900

    Update deps

--- denops/ddx/deps.ts
+++ denops/ddx/deps.ts
@@ -1,22 +1,22 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.6.0/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v3.6.0/helper/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.8.0/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v3.8.0/helper/mod.ts";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v3.6.0/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v3.6.0/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v3.6.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.6.0/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.6.0/autocmd/mod.ts";
+} from "https://deno.land/x/denops_std@v3.8.0/batch/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v3.8.0/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.8.0/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.8.0/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.8.0/autocmd/mod.ts";
 export {
   ensureArray,
   ensureObject,
   ensureString,
 } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.150.0/testing/asserts.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.150.0/path/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.151.0/testing/asserts.ts";
+export { parse, toFileUrl } from "https://deno.land/std@0.151.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.150.0/async/mod.ts";
+} from "https://deno.land/std@0.151.0/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v3.1.6/response_waiter.ts";
