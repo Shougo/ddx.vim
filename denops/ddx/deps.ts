2724c4c844b5ef8239baea11db2379bd4ee233d5
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-11-12 10:29:13 +0900

    Update deps

--- denops/ddx/deps.ts
+++ denops/ddx/deps.ts
@@ -1,15 +1,15 @@
-export type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v5.0.1/helper/mod.ts";
-export { batch } from "https://deno.land/x/denops_std@v5.0.1/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v5.0.1/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v5.0.1/autocmd/mod.ts";
-export { ensure, is } from "https://deno.land/x/unknownutil@v3.9.0/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.203.0/assert/mod.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.203.0/path/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v5.0.2/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v5.0.2/helper/mod.ts";
+export { batch } from "https://deno.land/x/denops_std@v5.0.2/batch/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v5.0.2/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v5.0.2/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v5.0.2/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v5.0.2/autocmd/mod.ts";
+export { ensure, is } from "https://deno.land/x/unknownutil@v3.10.0/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.206.0/assert/mod.ts";
+export { parse, toFileUrl } from "https://deno.land/std@0.206.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.203.0/async/mod.ts";
+} from "https://deno.land/std@0.206.0/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
