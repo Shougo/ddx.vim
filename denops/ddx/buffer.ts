856f5f44feb3dcf4876bac511dba78bd100eb30a
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-02-05 14:37:25 +0900

    Update deps

--- denops/ddx/buffer.ts
+++ denops/ddx/buffer.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "./deps.ts";
-import { ByteSliceStream } from "https://deno.land/std@0.210.0/streams/byte_slice_stream.ts";
-import { toArrayBuffer } from "https://deno.land/std@0.210.0/streams/to_array_buffer.ts";
+import { ByteSliceStream } from "https://deno.land/std@0.214.0/streams/byte_slice_stream.ts";
+import { toArrayBuffer } from "https://deno.land/std@0.214.0/streams/to_array_buffer.ts";
 
 
 type FileBuffer = {
--- denops/ddx/deps.ts
+++ denops/ddx/deps.ts
@@ -1,15 +1,15 @@
-export type { Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v5.2.0/helper/mod.ts";
-export { batch } from "https://deno.land/x/denops_std@v5.2.0/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v5.2.0/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.2.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v5.2.0/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v5.2.0/autocmd/mod.ts";
-export { ensure, is } from "https://deno.land/x/unknownutil@v3.11.0/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.209.0/assert/mod.ts";
-export { parse, toFileUrl } from "https://deno.land/std@0.209.0/path/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v6.0.1/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v6.0.1/helper/mod.ts";
+export { batch } from "https://deno.land/x/denops_std@v6.0.1/batch/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v6.0.1/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v6.0.1/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v6.0.1/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v6.0.1/autocmd/mod.ts";
+export { ensure, is } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.214.0/assert/mod.ts";
+export { parse, toFileUrl } from "https://deno.land/std@0.214.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.209.0/async/mod.ts";
+} from "https://deno.land/std@0.214.0/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
