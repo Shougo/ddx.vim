a1d46eea00d94acd863d8037640c242cdae2eb9b
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-05-04 11:43:27 +0900

    Use JSR modules

--- denops/ddx/deps.ts
+++ denops/ddx/deps.ts
@@ -5,15 +5,10 @@ export * as op from "https://deno.land/x/denops_std@v6.4.0/option/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts";
-export { ensure, is } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
-export {
-  basename,
-  parse,
-  toFileUrl,
-} from "https://deno.land/std@0.224.0/path/mod.ts";
-export {
-  deadline,
-  DeadlineError,
-} from "https://deno.land/std@0.224.0/async/mod.ts";
+
+export { assertEquals } from "jsr:@std/assert@0.224.0";
+export { basename, parse, toFileUrl } from "jsr:@std/path@0.224.0";
+export { deadline, DeadlineError } from "jsr:@std/async@0.224.0";
+
 export { Lock } from "https://deno.land/x/async@v2.1.0/mod.ts";
+export { ensure, is } from "https://deno.land/x/unknownutil@v3.18.0/mod.ts";
