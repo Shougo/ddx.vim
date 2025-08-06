09a2a99c6813591711a8aeb4e92009a4cb2b10e3
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-08-06 12:02:52 +0900

    Add import-map support in exts

--- denops/ddx/loader.ts
+++ denops/ddx/loader.ts
@@ -1,16 +1,30 @@
 import { BaseParams, DdxExtType, UiName } from "./types.ts";
 import { BaseUi } from "./base/ui.ts";
-import { isDenoCacheIssueError } from "./utils.ts";
+import { importPlugin, isDenoCacheIssueError } from "./utils.ts";
 
 import type { Denops } from "@denops/std";
 import * as op from "@denops/std/option";
 import * as fn from "@denops/std/function";
 
-import { Lock } from "@core/asyncutil/lock";
 import { basename } from "@std/path/basename";
+import { dirname } from "@std/path/dirname";
+import { join } from "@std/path/join";
 import { parse } from "@std/path/parse";
-import { toFileUrl } from "@std/path/to-file-url";
-import { is } from "@core/unknownutil/is";
+import { Lock } from "@core/asyncutil/lock";
+
+type Mod = {
+  // deno-lint-ignore no-explicit-any
+  mod: any;
+  path: string;
+};
+
+const PLUGIN_PREFIX = "@ddx";
+
+// Pattern for directories where auto-loadable extensions are placed by type
+const TYPE_DIR_PATTERN = `denops/${PLUGIN_PREFIX}-*s`;
+
+// Structured extension module entry point file.
+const EXT_ENTRY_POINT_FILE = "main.ts";
 
 export class Loader {
   #uis: Record<UiName, BaseUi<BaseParams>> = {};
@@ -19,7 +33,7 @@ export class Loader {
   };
   #checkPaths: Record<string, boolean> = {};
   #registerLock = new Lock(0);
-  #cachedPaths: Record<string, string> = {};
+  #cachedPaths = new Map<string, string>();
   #prevRuntimepath = "";
 
   async autoload(
@@ -29,31 +43,33 @@ export class Loader {
   ) {
     const runtimepath = await op.runtimepath.getGlobal(denops);
     if (runtimepath !== this.#prevRuntimepath) {
-      const cached = await globpath(
-        denops,
-        "denops/@ddx-*s",
-      );
+      const cachedPaths = await createPathCache(denops, runtimepath);
+
       // NOTE: glob may be invalid.
-      if (Object.keys(cached).length > 0) {
-        this.#cachedPaths = cached;
+      if (cachedPaths.size > 0) {
+        this.#cachedPaths = cachedPaths;
+        this.#prevRuntimepath = runtimepath;
       }
-      this.#prevRuntimepath = runtimepath;
     }
 
-    const key = `@ddx-${type}s/${this.getAlias(type, name) ?? name}`;
+    const key = `${PLUGIN_PREFIX}-${type}s/${
+      this.getAlias(type, name) ?? name
+    }`;
+    const path = this.#cachedPaths.get(key);
 
-    if (!this.#cachedPaths[key]) {
-      return;
+    if (!path) {
+      return this.#prevRuntimepath === "";
     }
 
-    await this.registerPath(type, this.#cachedPaths[key]);
+    await this.registerPath(type, path);
+    return true;
   }
 
   registerAlias(type: DdxExtType, alias: string, base: string) {
     this.#aliases[type][alias] = base;
   }
 
-  async registerPath(type: DdxExtType, path: string) {
+  async registerPath(type: DdxExtType, path: string): Promise<void> {
     await this.#registerLock.lock(async () => {
       try {
         await this.#register(type, path);
@@ -89,14 +105,29 @@ export class Loader {
     }
 
     const name = parse(path).name;
+    const mod: Mod = {
+      mod: undefined,
+      path,
+    };
 
-    const mod = await import(toFileUrl(path).href);
+    // NOTE: We intentionally use Deno.stat instead of safeStat here. We expect
+    // errors to be thrown when paths don't exist or are inaccessible.
+    const fileInfo = await Deno.stat(path);
+
+    if (fileInfo.isDirectory) {
+      // Load structured extension module
+      const entryPoint = join(path, EXT_ENTRY_POINT_FILE);
+      mod.mod = await importPlugin(entryPoint);
+    } else {
+      // Load single-file extension module
+      mod.mod = await importPlugin(path);
+    }
 
     let add;
     switch (type) {
       case "ui":
         add = (name: string) => {
-          const ui = new mod.Ui();
+          const ui = new mod.mod.Ui();
           ui.name = name;
           this.#uis[ui.name] = ui;
         };
@@ -117,34 +148,52 @@ export class Loader {
   }
 }
 
-async function globpath(
+async function createPathCache(
   denops: Denops,
-  search: string,
-): Promise<Record<string, string>> {
-  const runtimepath = await op.runtimepath.getGlobal(denops);
-
-  const paths: Record<string, string> = {};
-  const glob = await fn.globpath(
+  runtimepath: string,
+): Promise<Map<string, string>> {
+  const extFileGlob = await globpath(
     denops,
     runtimepath,
-    search + "/*.ts",
-    1,
-    1,
+    `${TYPE_DIR_PATTERN}/*.ts`,
+  );
+  const extDirEntryPointGlob = await globpath(
+    denops,
+    runtimepath,
+    `${TYPE_DIR_PATTERN}/*/${EXT_ENTRY_POINT_FILE}`,
   );
 
-  if (is.Array(glob)) {
-    // NOTE: glob may be invalid.
-    for (const path of glob) {
-      // Skip already added name.
-      const parsed = parse(path);
-      const key = `${basename(parsed.dir)}/${parsed.name}`;
-      if (key in paths) {
-        continue;
-      }
+  // Create key paths for both single-file and directory entry points.
+  // Prioritize the first occurrence key in keyPaths.
+  const keyPaths: Readonly<[key: string, path: string]>[] = [
+    //   1. `{name}.ts`
+    ...extFileGlob.map((extFile) => {
+      const { name, dir: typeDir } = parse(extFile);
+      const typeDirName = basename(typeDir);
+      const key = `${typeDirName}/${name}`;
+      return [key, extFile] as const;
+    }),
+    //   2. `{name}/main.ts`
+    ...extDirEntryPointGlob.map((entryPoint) => {
+      const extDir = dirname(entryPoint);
+      const { base: name, dir: typeDir } = parse(extDir);
+      const typeDirName = basename(typeDir);
+      const key = `${typeDirName}/${name}`;
+      return [key, extDir] as const;
+    }),
+  ];
 
-      paths[key] = path;
-    }
-  }
+  // Remove duplicate keys.
+  // Note that `Map` prioritizes the later value, so need to reversed.
+  const cache = new Map(keyPaths.toReversed());
 
-  return paths;
+  return cache;
+}
+
+async function globpath(
+  denops: Denops,
+  path: string,
+  pattern: string,
+): Promise<string[]> {
+  return await fn.globpath(denops, path, pattern, 1, 1) as unknown as string[];
 }
